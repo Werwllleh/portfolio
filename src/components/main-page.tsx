45f5dc309f36cfa336f31901396052e398acf366
Alexey A <allvxm4@gmail.com>
2026-02-16 21:19:57 +0300

    st3

--- src/components/main-page.tsx
+++ src/components/main-page.tsx
@@ -99,7 +99,7 @@ const MainPage = () => {
                     {ABOUT_INFO.CITY.TITLE}
                   </Link>
                   <p className="page-main__author--description">
-                    I build accessible, pixel-perfect digital experiences for the web.
+                    {ABOUT_INFO.DESCRIPTION}
                   </p>
                 </div>
                 <nav className="menu">
@@ -140,27 +140,13 @@ const MainPage = () => {
             <div className="page-main__right">
               <section id="about">
                 <div className="description">
-                  <p>I’m a frontend engineer with a specialty in web accessibility, focused on building pixel-perfect,
-                    intuitive user interfaces. I enjoy working at the intersection of design and engineering, where
-                    great
-                    user experience meets robust, clean, and scalable code.</p>
-                  <p>Currently, I'm a senior frontend engineer at Klaviyo, where I work on the component library team to
-                    help maintain and evolve our design system. In this role, I lead accessibility efforts across
-                    components, tooling, and patterns, partnering closely with designers and engineers to ensure
-                    accessibility is part of our core architecture.</p>
-                  <p>Previously, I’ve worked across a wide range of environments, from product studios to startups and
-                    large tech companies, including Apple, Starry Internet, and Upstatement. Alongside my professional
-                    work, I also created an online video course a few years ago which walks through building a
-                    real-world,
-                    API-driven application from scratch. These experiences have shaped how I think about building
-                    products
-                    that are both well-crafted and widely usable.</p>
+                  {ABOUT_INFO.TEXT}
                 </div>
               </section>
               <section id="experience">
                 {workList && workList.length ?
                   <ul className="work-list">
-                    {workList.map((data) => (
+                    {[...workList].reverse().map((data) => (
                       <li key={data.id}>
                         <WorkCard data={data}/>
                       </li>
